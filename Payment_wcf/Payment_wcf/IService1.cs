820d55bd39391c35f457d0a65aa9cb05558729b4
szabolcs <szabosz@kkszki.hu>
2022-10-17 10:33:32 +0200

    WCF GET adatok lekérése működik.

---
+++ Payment_wcf/Payment_wcf/IService1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace Payment_wcf
+{
+    
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            UriTemplate = "EgyCustomerAdata"
+            )]
+
+        Customer EgyCustomerGet();
+
+        [OperationContract]
+
+        Customer EgyCustomerAddCS();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            UriTemplate = "EgyCustomerHozzaad"
+            )]
+        Customer EgyCustomerAdd(Customer customer);
+
+        [OperationContract]
+
+        Customer EgyCustomerPostCS();
+
+        
+    }
+
+
+    [DataContract]
+    
+    public class Sor
+    {
+        [DataMember]
+
+        public int? ID { get; set; }
+    }
+
+    [DataContract]
+
+    public class Customer : Sor
+    {
+        [DataMember(IsRequired = true)]
+        public string Nev { get; set; }
+
+        [DataMember(IsRequired = true)]
+
+        public string Varos { get; set; }
+
+        [OperationContract]
+        public override string ToString()
+        {
+            return $"ID: {ID}\nNév: {Nev}\nVáros: {Varos}";
+        }
+    }
+}
---
+++ Payment_wcf/Payment_wcf/Service1.svc.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Web;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Payment_wcf
+{
+    
+    public class Service1 : IService1
+    {
+        public static List<Customer> customerList = new List<Customer>();
+        Random random = new Random();
+
+        public static HashSet<int> customerIndex = new HashSet<int>(); // ID helye
+
+        public static int Helyzet(int id)
+        {
+            int index = 0;
+            int sorSzama = customerList.Count;
+            while (index < sorSzama)
+            {
+                if (customerList[index].ID == id)
+                {
+                    return index;
+                }
+                index++;
+            }
+            return -1;
+        }
+
+        public Customer EgyCustomerGet()
+        {
+            Customer customer = new Customer();
+            customer.ID = 1;
+            customer.Nev = "Gipsz Jakab";
+            customer.Varos = "Miskolc";
+            Console.WriteLine("Adatok lekérése sikeres.");
+            return customer;
+
+        }
+
+        public Customer EgyCustomerGetCS()
+        {
+            return EgyCustomerGet();
+        }
+
+        public Customer EgyCustomerPost()
+        {
+            Customer customer = new Customer();
+            customer.ID = 2;
+            customer.Nev = "Minta Péter";
+            customer.Varos = "Budapest";
+            return customer;
+
+        }
+
+        public Customer EgyCustomerPostCS()
+        {
+            return EgyCustomerPost();
+        }
+
+        public List<Customer> CustomersLista()
+        {
+            return customerList;
+        }
+
+        public List<Customer> CustomersListajaCS()
+        {
+            return CustomersLista();
+        }
+    }
+}
