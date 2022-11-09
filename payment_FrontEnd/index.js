aa0a3ff7663d123328295d0891f0d5998792fa8c
szabolcs <szabosz@kkszki.hu>
2022-11-09 11:54:47 +0100

    POST,DELETE nem működik.

--- payment_FrontEnd/index.js
+++ payment_FrontEnd/index.js
@@ -1,27 +1,65 @@
-async function postCustomer(){
-
-    var nev = document.getElementById('nev').value;
-    var kor = document.getElementById('kor').value;
-    var varos = document.getElementById('varos').value;
-
-    var body = {
-        "Eletkor": kor,
-        "Nev": nev,
-        "Varos": varos
-    }
-
-    var url='http://localhost:3000/Service1.svc/CustomerPostDB';
-    var putUser=await fetch(url,{
-        method: "POST",
-        body: body,
-        headers: {
-            'Content-Type:': 'application/json'
-        }
-    })
+document.getElementById('button2').onclick=function (){
+    var delNumber=document.getElementById('number2').value;
+    delCustomer(delNumber)
 }
 
+async function delCustomer(delNumber){
+    var url='http://localhost:3000/Service1.svc/CustomerDeleteDB?Id={Id}/'+delNumber;
+    var delFetch = await fetch(url,{
+        method: "DELETE",
+        headers: {
+            'content-Type': 'application/json'
+        }
+    });
 
+    if (!delFetch.ok){
+        alert("DELETE végpont hiba!");
+        return;
+    }
 
+    var httpMessage=await delFetch.json();
+    //alert(httpMessage.stringify);
+    getCustomers();
+}
+
+document.getElementById('form1').onsubmit=function (event){
+  
+    event.preventDefault();
+    var nev=event.target.elements.nev.value;
+    var kor=event.target.elements.kor.value;
+    var varos=event.target.elements.varos.value;
+
+    var bodyCustomer=JSON.stringify({
+      Name: nev,
+      Age: kor,
+      City: varos
+    });
+
+    postCustomer(bodyCustomer);
+}
+
+async function postCustomer(bodyCustomer){
+   
+    var url='http://localhost:3000/Service1.svc/CustomerPostDB';
+    var postUser=await fetch(url,{
+        method: "POST",
+        body: bodyCustomer,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if(!postUser.ok){
+        alert("POST végpont hiba!");
+        return;
+    }
+
+    var httpMessage=await postUser.json();
+
+    alert(httpMessage);
+
+    getCustomers();
+} 
 
 async function getCustomers(){
 
